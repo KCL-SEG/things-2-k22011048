d1188fed351280c60c06c2a05ddf00f71089eecd
ayla <k22011048@kcl.ac.uk>
2023-11-27 14:33:11 +0000

    forms

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,20 @@
 """Forms of the project."""
+from django import forms
+from .models import Thing
 
+class ThingForm(forms.ModelForm):
+    class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity']
+        widgets = {
+            'description': forms.Textarea(),
+            'quantity': forms.NumberInput(attrs={'min': 0, 'max': 100}),
+
+        }
+
+    def clean_quantity(self):
+        quantity = self.cleaned_data['quantity']
+        if quantity < 0 or quantity > 100:
+            raise forms.ValidationError("Quantity must be between 0 and 100")
+        return quantity
 # Create your forms here.
